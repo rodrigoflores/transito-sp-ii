52d88662fcf938f9e22cf89600933990acb89261
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-04-21 13:02:08 -0300

    CSSing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def zone_name_for(zone)
+    case zone
+      when :north then "Zona Norte"
+      when :west then "Zona Oeste"
+      when :south then "Zona Sul"
+      when :east then "Zona Leste"
+      when :downtown then "Centro"
+    end
+  end
 end
