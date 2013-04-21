d62ab242dc360e1d95e4a44001cdd3e8ba76cdf8
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-04-21 18:46:29 -0300

    Added the traffic action to json

--- app/controllers/traffic_controller.rb
+++ app/controllers/traffic_controller.rb
@@ -1,6 +1,28 @@
 class TrafficController < ApplicationController
+  respond_to :html, :json
+
   def index
     @overall_traffic = Traffic::Scraper.overall_traffic
     @per_zone_traffic = Traffic::Scraper.zone_traffic
+
+    respond_to do |format|
+      format.html
+      format.json { render json: translate_hash }
+    end
+  end
+
+  private
+
+  def translate_hash
+    {
+      zonas: {
+        norte: @per_zone_traffic[:north],
+        sul: @per_zone_traffic[:south],
+        leste: @per_zone_traffic[:east],
+        oeste: @per_zone_traffic[:west],
+        centro: @per_zone_traffic[:downtown]
+      },
+      total: @overall_traffic
+    }
   end
 end
