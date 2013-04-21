ad477dd4d64e22ce49d6118c6535d1fbd343d221
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-04-21 15:56:26 -0300

    Improving test cases

--- spec/features/traffic_spec.rb
+++ spec/features/traffic_spec.rb
@@ -2,19 +2,19 @@ require "spec_helper"
 
 feature 'Traffic Information', :vcr do
   scenario 'shows the overall and traffic for each zone' do
-    Traffic::Scraper.should_receive(:overall_traffic).and_return(4)
+    Traffic::Scraper.should_receive(:overall_traffic).and_return(15)
     Traffic::Scraper.should_receive(:zone_traffic).and_return({
-      north: 0,
+      north: 1,
       south: 2,
-      east: 0,
-      west: 0,
-      downtown: 2
+      east: 3,
+      west: 4,
+      downtown: 5
     })
 
     visit '/'
 
     within(".north") do
-      page.should have_text("Norte 0 km")
+      page.should have_text("Norte 1 km")
     end
 
     within(".south") do
@@ -22,19 +22,19 @@ feature 'Traffic Information', :vcr do
     end
 
     within(".east") do
-      page.should have_text("Leste 0 km")
+      page.should have_text("Leste 3 km")
     end
 
     within(".west") do
-      page.should have_text("Oeste 0 km")
+      page.should have_text("Oeste 4 km")
     end
 
     within(".downtown") do
-      page.should have_text("Centro 2 km")
+      page.should have_text("Centro 5 km")
     end
 
     within(".overall") do
-      page.should have_text("Total 4 km")
+      page.should have_text("Total 15 km")
     end
   end
 end
