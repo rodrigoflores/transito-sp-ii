c4c374dc68a7d6cfcc4774097ea5c940320c06f0
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-04-21 08:22:57 -0300

    Added a bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransitoSpIi::Application.config.secret_token = '96bd0dae304e4bdc52dc74a709dba6ce7d8a79ee071a8d0c4d37407ea1e02b4a5a134b51a279a5eeb297c5a5658e78279180fe9fd7af56389462aca8998080bf'
