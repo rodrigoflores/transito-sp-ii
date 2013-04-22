f0a78a9004624ddb6a5e97ccee6e9f0948218cb3
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-04-21 23:09:40 -0300

    Trying to use ENV

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,4 +1,5 @@
 config = YAML.load_file(Rails.root.join('config', 'redis.yml'))
 REDIS_URI = URI.parse(config[Rails.env]['path'])
+REDIS_URI = URI.parse(ENV['REDISTOGO_URL']) if ENV['REDISTOGO_URL']
 
 Storage.setup
