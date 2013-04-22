2528d679a8f730c29e354bb4818dabb506f74a29
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2013-04-21 23:01:06 -0300

    No exceptions for production

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,8 +1,4 @@
-if Rails.env.production? && ENV["REDISTOGO_URL"]
-  REDIS_URI = URI.parse(ENV["REDISTOGO_URL"])
-else
-  config = YAML.load_file(Rails.root.join('config', 'redis.yml'))
-  REDIS_URI = URI.parse(config[Rails.env]['path'])
-end
+config = YAML.load_file(Rails.root.join('config', 'redis.yml'))
+REDIS_URI = URI.parse(config[Rails.env]['path'])
 
 Storage.setup
